6a20d9b7709f4fa6daaabef0714718395e89230f
haniszewski <ka.haniszewski@gmail.com>
2023-04-28 12:54:32 -0400

    Initial fix in dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-const express = require('express');
-const ormConfig = require('../ormconfig.json');
-const {createConnection} = require('typeorm');
+import express, { Request, Response } from 'express';
+import ormConfig from '../ormconfig.json';
+import { createConnection, DataSourceOptions } from 'typeorm';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-createConnection(ormConfig)
+createConnection(ormConfig as DataSourceOptions)
   .then(() => {
     app.listen(3000, () => {
       console.log('Example app listening on port 3000!');
     });
   })
-  .catch((error) => {
+  .catch((error: Error) => {
     console.log(error);
   });
